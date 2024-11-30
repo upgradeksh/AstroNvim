666d510fac7b6ef4fde1fa4517ad85db078f0225
john.kang <upgradeksh@gmail.com>
2024-12-01 02:37:33 +0900

    feat(markdown): 마크다운 랜더러 추가.

--- lua/community.lua
+++ lua/community.lua
@@ -13,6 +13,7 @@ return {
   -- 설치전 nodejs, yarn 이 설치 되어 있어야 함.
   -- 플러그인 설치가 먼저 되었다면 의존성 설치후 재설치 해야함
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
   { import = "astrocommunity.motion.marks-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.pack.ansible" },
