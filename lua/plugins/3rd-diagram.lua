76b26ade8c0b65bb0de7e7f039a5c6abe95e7eb6
john.kang <upgradeksh@gmail.com>
2024-12-05 09:29:30 +0900

    feat(markdown): mermaid render 추가
    mmdc 바이너리를 설치 해야 합니다.
    
    ```bash
    npm install -g @mermaid-js/mermaid-cli
    ```

---
+++ lua/plugins/3rd-diagram.lua
@@ -0,0 +1,27 @@
+-- mmdc 바이너리를 설치 합니다.
+-- npm install -g @mermaid-js/mermaid-cli
+return {
+  "3rd/diagram.nvim",
+  dependencies = {
+    "3rd/image.nvim",
+  },
+  opts = { -- you can just pass {}, defaults below
+    renderer_options = {
+      mermaid = {
+        background = nil, -- nil | "transparent" | "white" | "#hex"
+        theme = nil, -- nil | "default" | "dark" | "forest" | "neutral"
+        scale = 3, -- nil | 1 (default) | 2  | 3 | ...
+      },
+      plantuml = {
+        charset = nil,
+      },
+      d2 = {
+        theme_id = nil,
+        dark_theme_id = nil,
+        scale = nil,
+        layout = nil,
+        sketch = nil,
+      },
+    },
+  },
+}
